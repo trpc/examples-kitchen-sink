b90230bdfd3de321c39bf38cecfb2c1765a479c3
Dylan Brookes <24858006+merelinguist@users.noreply.github.com>
2021-11-29 22:14:46 +0000

    Add copy to clipboard to code examples (#3)

---
+++ src/utils/useClipboard.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+export const useClipboard = (text: string): [boolean, () => void] => {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  useEffect(() => {
+    if (hasCopied) {
+      const id = setTimeout(() => {
+        setHasCopied(false);
+      }, 600);
+
+      return () => {
+        clearTimeout(id);
+      };
+    }
+  }, [hasCopied]);
+
+  return [
+    hasCopied,
+    () => {
+      navigator.clipboard.writeText(text).then(() => setHasCopied(true));
+    },
+  ];
+};

490591fbeec3af2fca4dd4855cc282675b2d66f3
Marco Sampellegrini <babbonatale@alpacaaa.net>
2021-12-08 12:27:00 +0100

    Remove redundant validation schema (#5)

--- src/pages/react-hook-form.tsx
+++ src/pages/react-hook-form.tsx
@@ -1,13 +1,6 @@
 import FeaturePage from 'feature/react-hook-form';
 import { meta } from 'feature/react-hook-form/meta';
 import { ExamplePage } from 'utils/ExamplePage';
-import { z } from 'zod';
-
-// validation schema is used by server
-export const validationSchema = z.object({
-  title: z.string().min(2),
-  text: z.string().min(5),
-});
 
 export default function Page() {
   return (

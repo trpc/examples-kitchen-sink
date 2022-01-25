cce41d0cf2a8aaaa665747b1d4fe88c21b396516
ShiftySlothe <forson.charlesj@gmail.com>
2022-01-25 11:54:55 +0000

    Added next-auth session to tRPC Router Context

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import * as trpc from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+import { getSession } from 'next-auth/react';
 
 const prisma = new PrismaClient({
   log:
@@ -12,9 +14,17 @@ const prisma = new PrismaClient({
  * Creates context for an incoming request
  * @link https://trpc.io/docs/context
  */
-export const createContext = async () => {
+export const createContext = async (
+  opts?: trpcNext.CreateNextContextOptions,
+) => {
+  const req = opts?.req;
+  const res = opts?.res;
+  const session = await getSession({ req });
   // for API-response caching see https://trpc.io/docs/caching
   return {
+    req,
+    res,
+    session,
     prisma,
   };
 };

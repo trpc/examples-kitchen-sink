41341fc8577d022ccbe08c708a8ba1383d1d4c2f
KATT <alexander@n1s.se>
2022-02-01 22:39:00 +0100

    tweak

--- src/server/context.ts
+++ src/server/context.ts
@@ -17,7 +17,7 @@ const prisma = new PrismaClient({
  * @link https://trpc.io/docs/context
  */
 export const createContext = async (
-  opts: trpcNext.CreateNextContextOptions,
+  opts?: trpcNext.CreateNextContextOptions,
 ) => {
   const req = opts?.req;
   const res = opts?.res;
@@ -28,7 +28,7 @@ export const createContext = async (
    * Note: If no req -> SSG is being used -> no session exists (null)
    * @link https://github.com/nextauthjs/next-auth/issues/1535
    */
-  const session = await getServerSession(opts, nextAuthOptions);
+  const session = opts && (await getServerSession(opts, nextAuthOptions));
 
   // for API-response caching see https://trpc.io/docs/caching
   return {

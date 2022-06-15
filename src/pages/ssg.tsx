3ce1e846354c6d45d701d1a504eeba5f8c33a1fe
Chris Bautista <chrisbautistaaa@gmail.com>
2022-06-14 22:58:15 -0700

    fix: statis props inference

--- src/pages/ssg.tsx
+++ src/pages/ssg.tsx
@@ -1,14 +1,14 @@
 import { createSSGHelpers } from '@trpc/react/ssg';
 import { meta } from 'feature/ssg/meta';
-import { InferGetStaticPropsType } from 'next';
 import { createContext } from 'server/context';
 import { appRouter } from 'server/routers/_app';
 import superjson from 'superjson';
 import { ExamplePage } from 'utils/ExamplePage';
+import { inferSSGProps } from 'utils/inferSSGProps';
 import { trpc } from 'utils/trpc';
 
 export default function Page(
-  props: InferGetStaticPropsType<typeof getStaticProps>,
+  props: inferSSGProps<typeof getStaticProps>,
 ) {
   const { id } = props;
   const query = trpc.useQuery(['ssg.byId', { id }]);
